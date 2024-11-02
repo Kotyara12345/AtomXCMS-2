c545c8892715a0868e225f4d1dce44fef5a542e9
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2024-11-02 10:18:54 +0300

    PHP8.3, улучшена читаемость

--- admin/clean_cache.php
+++ admin/clean_cache.php
@@ -1,33 +1,40 @@
 <?php
 /*-----------------------------------------------\
-| 												 |
-| @Author:       Andrey Brykin (Drunya)          |
-| @Email:        drunyacoder@gmail.com           |
-| @Site:         http://atomx.net                |
-| @Version:      1.4                             |
-| @Project:      CMS                             |
-| @package       CMS AtomX                       |
-| @subpackege    Admin Panel module  			 |
-| @copyright     ©Andrey Brykin 2010-2013        |
+|  Author:       Andrey Brykin (Drunya)          |
+|  Email:        drunyacoder@gmail.com           |
+|  Site:         http://atomx.net                |
+|  Version:      1.4                             |
+|  Project:      CMS                             |
+|  package       CMS AtomX                       |
+|  subpackage    Admin Panel module              |
+|  copyright     ©Andrey Brykin 2010-2013        |
 \-----------------------------------------------*/
 
 /*-----------------------------------------------\
-| 												 |
 |  any partial or not partial extension          |
-|  CMS AtomX,without the consent of the          |
+|  CMS AtomX, without the consent of the        |
 |  author, is illegal                            |
 |------------------------------------------------|
 |  Любое распространение                         |
 |  CMS AtomX или ее частей,                      |
-|  без согласия автора, является не законным     |
+|  без согласия автора, является незаконным      |
 \-----------------------------------------------*/
 
 include_once '../sys/boot.php';
 include_once ROOT . '/admin/inc/adm_boot.php';
 
+// Очищаем кеш
 $_SESSION['message'] = __('Cache is cleared');
 $Register['Cache']->clean();
-$Snippets = new AtmSnippets;
-$Snippets->cleanCahe();
-_unlink(ROOT . '/sys/cache/templates/');
+
+$snippets = new AtmSnippets();
+$snippets->cleanCache();
+
+// Удаляем кешированные шаблоны
+$cachePath = ROOT . '/sys/cache/templates/';
+if (is_dir($cachePath)) {
+    array_map('unlink', glob("$cachePath/*.*"));
+    rmdir($cachePath);
+}
+
 redirect('/admin');
