c504e248d8a7d9fb9cc390fd0ae547af1089e92c
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2024-11-02 10:54:43 +0300

    PHP8.3, улучшена читаемость

--- index.php
+++ index.php
@@ -5,55 +5,53 @@
 |  @Version:      1.2.9                          |
 |  @Project:      CMS                            |
 |  @package       CMS AtomX                      |
-|  @subpackege    Entry dot                      |
+|  @subpackage    Entry dot                      |
 |  @copyright     ©Andrey Brykin 2010-2012       |
 |  @last mod.     2012/04/29                     |
 \-----------------------------------------------*/
 
 /*-----------------------------------------------\
 | 												 |
-|  any partial or not partial extension          |
-|  CMS AtomX,without the consent of the          |
+|  Any partial or not partial extension          |
+|  CMS AtomX, without the consent of the         |
 |  author, is illegal                            |
 |------------------------------------------------|
 |  Любое распространение                         |
 |  CMS AtomX или ее частей,                      |
-|  без согласия автора, является не законным     |
+|  без согласия автора, является незаконным      |
 \-----------------------------------------------*/
 
-
-
-
+// Set the content type and charset
 header('Content-Type: text/html; charset=utf-8');
 
-
-
-
+// Check if the installation directory exists
 if (file_exists('install')) {
-	include_once ('sys/settings/config.php');
-	if (!empty($set) &&
+    include_once('sys/settings/config.php');
+    
+    // Validate configuration settings
+    if (!empty($set) &&
         !empty($set['db']['name']) &&
-        (
-            !empty($set['db']['user']) ||
-            !empty($set['db']['pass'])
-        )
+        (!empty($set['db']['user']) || !empty($set['db']['pass']))
     ) {
-		die('Before use your site, delete INSTALL dir! <br />Перед использованием удалите папку INSTALL');	
-	}
-	header('Location: install'); die();
+        die('Before using your site, delete the INSTALL directory! <br />Перед использованием удалите папку INSTALL');    
+    }
+    
+    // Redirect to the install page if the installation directory exists
+    header('Location: install');
+    exit;
 }
-
-
-
 
 include_once 'sys/boot.php';
 
-Plugins::intercept('before_pather', array());
+// Intercept before routing
+Plugins::intercept('before_pather', []);
 
 /**
  * Parser URL
  * Get params from URL and launch needed module and action
  */
 new Pather($Register);
-Plugins::intercept('after_pather', array());
+
+// Intercept after routing
+Plugins::intercept('after_pather', []);
 //pr($Register);
