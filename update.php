95fd7fceb9c196b52c3a2b585f8a7c3456ace0fd
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2024-11-02 10:49:30 +0300

    Использование массивов, циклов, улучшена безопастность

--- update.php
+++ update.php
@@ -2,46 +2,58 @@
 include_once '/sys/boot.php';
 include '/sys/settings/acl_rules.php';
 
+// Массив с запросами на переименование таблиц
+$renameTables = [
+    'news_sections' => 'news_categories',
+    'foto_sections' => 'foto_categories',
+    'loads_sections' => 'loads_categories',
+    'stat_sections' => 'stat_categories'
+];
 
-// convert section tables
-$Register['DB']->query("RENAME TABLE `news_sections` TO `news_categories`");
-$Register['DB']->query("RENAME TABLE `foto_sections` TO `foto_categories`");
-$Register['DB']->query("RENAME TABLE `loads_sections` TO `loads_categories`");
-$Register['DB']->query("RENAME TABLE `stat_sections` TO `stat_categories`");
+// Переименование таблиц
+foreach ($renameTables as $oldName => $newName) {
+    $Register['DB']->query("RENAME TABLE `$oldName` TO `$newName`");
+}
 
+// Добавление поддержки кириллицы
+$tablesToAlter = ['news', 'stat', 'loads', 'themes', 'foto'];
+foreach ($tablesToAlter as $table) {
+    $Register['DB']->query("ALTER TABLE `$table` ADD `clean_url_title` VARCHAR(255) DEFAULT '' NOT NULL");
+}
 
-// Add cyrillic  support
-$Register['DB']->query("ALTER TABLE news ADD clean_url_title VARCHAR(255) DEFAULT '' NOT NULL");
-$Register['DB']->query("ALTER TABLE stat ADD clean_url_title VARCHAR(255) DEFAULT '' NOT NULL");
-$Register['DB']->query("ALTER TABLE loads ADD clean_url_title VARCHAR(255) DEFAULT '' NOT NULL");
-$Register['DB']->query("ALTER TABLE themes ADD clean_url_title VARCHAR(255) DEFAULT '' NOT NULL");
-$Register['DB']->query("ALTER TABLE foto ADD clean_url_title VARCHAR(255) DEFAULT '' NOT NULL");
+// Обновление пользователей
+$Register['DB']->query("ALTER TABLE `users` ADD `first_name` VARCHAR(32) CHARACTER SET utf8 NOT NULL DEFAULT ''");
+$Register['DB']->query("ALTER TABLE `users` ADD `last_name` VARCHAR(32) CHARACTER SET utf8 NOT NULL DEFAULT ''");
 
-// Update users
-$Register['DB']->query("ALTER TABLE `users` ADD `first_name` VARCHAR(32) character set utf8 NOT NULL default ''");
-$Register['DB']->query("ALTER TABLE `users` ADD `last_name` VARCHAR(32) character set utf8 NOT NULL default ''");
+// Добавление индексов
+$indexesToAdd = [
+    'forums' => ['in_cat'],
+    'loads' => ['category_id', 'author_id'],
+    'foto' => ['category_id', 'author_id'],
+    'news' => ['category_id', 'author_id'],
+    'stat' => ['category_id', 'author_id'],
+    'comments' => ['entity_id', 'user_id'],
+    'messages' => ['to_user', 'from_user'],
+    'posts' => ['id_theme'],
+    'themes' => ['id_forum'],
+    'users_votes' => ['from_user', 'to_user'],
+    'loads_add_content' => ['field_id', 'entity_id'],
+    'news_add_content' => ['field_id', 'entity_id'],
+    'stat_add_content' => ['field_id', 'entity_id'],
+    'users_add_content' => ['field_id', 'entity_id'],
+    'users_warnings' => ['user_id', 'admin_id'],
+    'loads_attaches' => ['user_id'],
+    'news_attaches' => ['user_id'],
+    'stat_attaches' => ['user_id'],
+    'forum_attaches' => ['post_id', 'user_id']
+];
 
+// Добавление индексов в таблицы
+foreach ($indexesToAdd as $table => $indexes) {
+    foreach ($indexes as $index) {
+        $Register['DB']->query("ALTER TABLE `$table` ADD INDEX (`$index`)");
+    }
+}
 
-// Add indexes
-$Register['DB']->query("ALTER TABLE `forums` ADD INDEX (`in_cat`)");
-$Register['DB']->query("ALTER TABLE `loads` ADD INDEX (`category_id`, `author_id`)");
-$Register['DB']->query("ALTER TABLE `foto` ADD INDEX (`category_id`, `author_id`)");
-$Register['DB']->query("ALTER TABLE `news` ADD INDEX (`category_id`, `author_id`)");
-$Register['DB']->query("ALTER TABLE `stat` ADD INDEX (`category_id`, `author_id`)");
-$Register['DB']->query("ALTER TABLE `comments` ADD INDEX (`entity_id`, `user_id`)");
-$Register['DB']->query("ALTER TABLE `messages` ADD INDEX (`to_user`, `from_user`)");
-$Register['DB']->query("ALTER TABLE `posts` ADD INDEX (`id_theme`)");
-$Register['DB']->query("ALTER TABLE `themes` ADD INDEX (`id_forum`)");
-$Register['DB']->query("ALTER TABLE `users_votes` ADD INDEX (`from_user`, `to_user`)");
-$Register['DB']->query("ALTER TABLE `loads_add_content` ADD INDEX (`field_id`, `entity_id`)");
-$Register['DB']->query("ALTER TABLE `news_add_content` ADD INDEX (`field_id`, `entity_id`)");
-$Register['DB']->query("ALTER TABLE `stat_add_content` ADD INDEX (`field_id`, `entity_id`)");
-$Register['DB']->query("ALTER TABLE `users_add_content` ADD INDEX (`field_id`, `entity_id`)");
-$Register['DB']->query("ALTER TABLE `users_warnings` ADD INDEX (`user_id`, `admin_id`)");
-$Register['DB']->query("ALTER TABLE `loads_attaches` ADD INDEX (`user_id`)");
-$Register['DB']->query("ALTER TABLE `news_attaches` ADD INDEX (`user_id`)");
-$Register['DB']->query("ALTER TABLE `stat_attaches` ADD INDEX (`user_id`)");
-$Register['DB']->query("ALTER TABLE `forum_attaches` ADD INDEX (`post_id`, `user_id`)");
-
-
+// Завершение операции
 die(__('Operation is successful'));
