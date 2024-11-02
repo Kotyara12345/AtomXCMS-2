8f8f283c7197ee654d15d32301560e5ec21ced0c
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2024-11-02 10:44:48 +0300

    PHP8.3, улучшена читаемость

--- install/ping.php
+++ install/ping.php
@@ -3,26 +3,25 @@
 if (empty($_GET['type'])) $_GET['type'] = true;
 
 if ($_GET['type'] === true) {
-	checkRequest();
+    checkRequest();
 } else {
-	checkUpdate();
+    checkUpdate();
 }
 
-
 function checkUpdate() {
-	@$b = file_get_contents('http://home.atomx.net/cdn/versions.txt');
-	if ($b) {
-		if (preg_match('#[^></]+#i', $b)) {
-			echo '<a target="_blank" href="https://github.com/Drunyacoder/AtomXCMS-2/releases">Последняя версия ' . trim($b) . '</a>';
-		} else {
-			echo 'Не удалось узнать';
-		}
-	} else {
-		echo 'Не удалось узнать';
-	}
+    @$b = file_get_contents('http://home.atomx.net/cdn/versions.txt');
+    if ($b) {
+        if (preg_match('#[^></]+#i', $b)) {
+            echo '<a target="_blank" href="https://github.com/Drunyacoder/AtomXCMS-2/releases">Последняя версия ' . trim($b) . '</a>';
+        } else {
+            echo 'Не удалось узнать';
+        }
+    } else {
+        echo 'Не удалось узнать';
+    }
 }
 
 function checkRequest() {
-	@$b = file_get_contents('http://home.atomx.net/check.php?v=2.7.0Beta&d=' . $_SERVER['HTTP_HOST']);
+    @$b = file_get_contents('http://home.atomx.net/check.php?v=2.7.0Beta&d=' . $_SERVER['HTTP_HOST']);
 }
 ?>
