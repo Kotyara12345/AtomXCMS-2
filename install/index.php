d7d98664705d2340abdfbc185c2ce8edd878ff66
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2024-11-02 10:40:06 +0300

    Оптимизация под php8.3, html5, css3

--- install/index.php
+++ install/index.php
@@ -1,43 +1,39 @@
-﻿<!doctype html>
-<html>
+<!doctype html>
+<html lang="ru">
 <head>
-<title>AtomX CMS - вместе в будущее</title>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<link rel="shortcut icon" href="../sys/img/favicon.ico" type="image/x-icon">
-<link type="text/css" rel="StyleSheet" href="css/style.css" />
-<script language="JavaScript" type="text/javascript" src="../sys/js/jquery.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AtomX CMS - вместе в будущее</title>
+    <link rel="icon" href="../sys/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="../sys/js/jquery.js" defer></script>
 </head>
 <body>
-<div id="head">
-	<div id="logo"></div>
-</div>
-<div id="subhead"></div>
+    <header id="head">
+        <div id="logo" aria-label="Логотип AtomX CMS"></div>
+    </header>
 
-<div id="container">
-	<div id="descr">
-		<h2>Внимание!</h2>
-		CMS AtomX предназначена, лишь, для личного использования и не должна распространяться от Вас
-		третьим лицам. <br /><br />
-		
-		Копирование или распространение CMS AtomX или ее частей без согласия автора (Brykin Andrey) 
-		считается не законным. <br /><br />
-		
-		Вы в праве редактировать и изменять Вашу копию программы по своему усмотрению.<br /><br />
-		
-		Если данная копия программы досталась Вам не от официального источника (сайт <a href="http://atomx.net">http://atomx.net</a>), просим сообщить об этом нам (например на нашем форуме - <a href="http://atomx.net/forum/">http://atomx.net/forum</a>) и получить в замен лицензионную копию программы.<br /><br />
-		
-		Предупреждаем, что использование не лицензионных программ, может повлечь проблемы и являеться не безопасным.
-	</div>
-	<a id="begin" href="step1.php">ПОЕХАЛИ</a>
-</div>
-<div id="footer1">
-	<div style="float:left;">
-		<a href="http://atomx.net/">OFF. SITE</a>
-		<a href="http://home.atomx.net/">ABOUT</a>
-		<a href="http://atomx.net/forum/">SUPPORT</a>
-		<a href="http://atomx.net/documa/">WIKI</a>
-	</div>
-	<div style="clear:both;"></div>
-</div>
+    <div id="subhead"></div>
+
+    <main id="container">
+        <section id="descr">
+            <h2>Внимание!</h2>
+            <p>CMS AtomX предназначена лишь для личного использования и не должна распространяться третьим лицам.</p>
+            <p>Копирование или распространение CMS AtomX или ее частей без согласия автора (Brykin Andrey) считается незаконным.</p>
+            <p>Вы в праве редактировать и изменять Вашу копию программы по своему усмотрению.</p>
+            <p>Если данная копия программы досталась Вам не от официального источника (сайт <a href="http://atomx.net">atomx.net</a>), просим сообщить об этом нам (например, на нашем форуме - <a href="http://atomx.net/forum/">atomx.net/forum</a>) и получить в замен лицензионную копию программы.</p>
+            <p>Предупреждаем, что использование нелицензионных программ может повлечь проблемы и является небезопасным.</p>
+        </section>
+        <a id="begin" class="button" href="step1.php">ПОЕХАЛИ</a>
+    </main>
+
+    <footer id="footer1">
+        <nav>
+            <a href="http://atomx.net/">OFF. SITE</a>
+            <a href="http://home.atomx.net/">ABOUT</a>
+            <a href="http://atomx.net/forum/">SUPPORT</a>
+            <a href="http://atomx.net/documa/">WIKI</a>
+        </nav>
+    </footer>
 </body>
 </html>
