a588315a499de6a47b54d339a78e123dab8d60d2
Kotyara12345 <117313600+Kotyara12345@users.noreply.github.com>
2025-09-08 13:55:58 +0300

    Update adm_boot.php

--- admin/inc/adm_boot.php
+++ admin/inc/adm_boot.php
@@ -1,6 +1,9 @@
 <?php
+
+declare(strict_types=1);
+
 ##################################################
-##												##
+##                                              ##
 ## @Author:       Andrey Brykin (Drunya)        ##
 ## @Version:      1.3                           ##
 ## @Project:      CMS                           ##
@@ -10,9 +13,8 @@
 ## @Last mod.     2014/01/10                    ##
 ##################################################
 
-
 ##################################################
-##												##
+##                                              ##
 ## any partial or not partial extension         ##
 ## CMS AtomX,without the consent of the         ##
 ## author, is illegal                           ##
@@ -22,207 +24,226 @@
 ## без согласия автора, является не законным    ##
 ##################################################
 
-
 header('Content-Type: text/html; charset=utf-8');
-if (!isInstall()) redirect('/install');
 
+if (!isInstall()) {
+    redirect('/install');
+}
 
+$FpsDB = $Register['DB'] ?? null;
+$ACL = $Register['ACL'] ?? null;
 
+if (!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = Config::read('language');
+}
 
-$FpsDB = $Register['DB']; //TODO
-$ACL = $Register['ACL'];
-$_SESSION['lang'] = Config::read('language');
-
-/*
-pr(time());
-pr($_SESSION['adm_panel_authorize']);
-die();
-*/
-
-
+// Проверка реферера
 if (ADM_REFER_PROTECTED == 1) {
-	$script_name = (!empty($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
-	$script_name = strrchr($script_name, '/');
-	if ($script_name != '/index.php') {
-		$referer = (!empty($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '';
-		preg_match('#^http://([^/]+)#', $referer, $match);
-		if (empty($match[1]) || $match[1] != $_SERVER['SERVER_NAME'])
-			redirect('/admin/index.php');
-	}
+    $script_name = $_SERVER['REQUEST_URI'] ?? '';
+    $script_name = strrchr($script_name, '/');
+    
+    if ($script_name !== '/index.php') {
+        $referer = $_SERVER['HTTP_REFERER'] ?? '';
+        preg_match('#^https?://([^/]+)#', $referer, $match);
+        
+        if (empty($match[1]) || $match[1] !== $_SERVER['SERVER_NAME']) {
+            redirect('/admin/index.php');
+        }
+    }
 }
 
-
-///if (empty($_SESSION['user'])) redirect('/');
-if (!isset($_SESSION['adm_panel_authorize']) || $_SESSION['adm_panel_authorize'] < time() || empty($_SESSION['user'])) {
-	if (isset($_POST['send']) && isset($_POST['login']) && isset($_POST['passwd'])) {
-		$errors = '';
-		$login = strtolower(trim($_POST['login']));
-		$pass = trim($_POST['passwd']);
-		
-		if (empty($login)) $errors .= '<li>Заполните поле "Логин"</li>';
-		if (empty($pass)) $errors .= '<li>Заполните поле "Пароль"</li>';
-		
-
-		if (empty($errors)) {
-			
-			$user = $FpsDB->select('users', DB_FIRST, array('cond' => array('name' => $login, 'passw' => md5($pass))));
-			if (!count($user)) {
-				$errors .= '<li>Не верный Пароль или Логин</li>';
-			} else {
-				//turn access
-				$ACL->turn(array('panel', 'entry'), true, $user[0]['status']);
-			}
-			
-			if (empty($errors)) {
-				$_SESSION['user'] = $user[0];
-				$_SESSION['adm_panel_authorize'] = (time() + Config::read('session_time', 'secure'));
-				redirect('/admin/');
-			}
-		}
-	}
-
-
-    $pageTitle = 'Авторизация в панели Администрирования';
-    $pageNav = '';
-    $pageNavr = '';
-
-
-?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>AtomX Admin Panel Authorization</title>
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-	<link rel="StyleSheet" type="text/css" href="<?php echo WWW_ROOT ?>/admin/template/css/style.css" />
-	<script language="JavaScript" type="text/javascript" src="<?php echo WWW_ROOT ?>/sys/js/jquery.js"></script>
-	<script type="text/javascript">
-	</script>
-</head>
-<body>
-	<script>
-	$(document).ready(function(){
-		var shmask = $('.shadow-mask');
-		if (typeof shmask != 'undefined') {
-		
-			var body = $('body').css('width');
-			var lpos = (parseInt(body) - 900) / 2;
-			if (lpos < 1) lpos = 0;
-			
-			var l = (lpos % 18);
-			l = lpos + (18 - l) + 51;
-			shmask.css({'left': l});
-			
-			/*
-			var t = (parseInt(shmask.css('top')) % 10);
-			t = parseInt(shmask.css('top')) + t;
-			shmask.css({'top': t});
-			*/
-		}
-	});
-	</script>
-	<div id="login-wrapper">
-		<div class="shadow-mask"></div>
-		<div class="form">
-			<div class="title">Авторизация</div>
-			<form method="POST" action="" >
-				<div class="items">
-					<?php 
-					if (!empty($errors)) {
-						echo '<ul class="error">' . $errors . '</ul>';
-						unset($errors);
-					}
-					?>
-					<div class="item"><span>Логин</span><input name="login" type="text" /></div>
-					<div class="item"><span>Пароль</span><input name="passwd" type="password" /></div>
-				</div>
-				<div class="submit"><input type="submit" name="send" value="" /></div>
-			</form>
-		</div>
-	</div>
-</body>
-</html>
-
-
-
-
-
-
-<?php	
-	//include_once 'template/footer.php';
-	die();
-
-	
-	
-} else if (!empty($_SESSION['adm_panel_authorize'])) {
-	$_SESSION['adm_panel_authorize'] = (time() + Config::read('session_time', 'secure'));
-	
-	
-	if (!empty($ACL)) $ACL = $Register['ACL'];
-	
-	if ($ACL->turn(array('panel', 'restricted_access'), false)) {
-
-		$url = preg_replace('#^.*/([^/]+)\.\w{2,5}$#i', "$1", $_SERVER['SCRIPT_NAME']);
-		
-		if (!empty($url) && $url != 'index' && $url != 'exit') {
-			if (!$ACL->turn(array('panel', 'restricted_access_' . $url), false)) {
-				$_SESSION['message'] = __('Permission denied');
-				redirect('/admin/');
-			}
-		}
-	}
+// Проверка авторизации
+if (!isset($_SESSION['adm_panel_authorize']) 
+    || $_SESSION['adm_panel_authorize'] < time() 
+    || empty($_SESSION['user'])
+) {
+    handleLoginForm($FpsDB, $ACL);
+    die();
 }
 
+// Обновление времени сессии
+$_SESSION['adm_panel_authorize'] = time() + (int)Config::read('session_time', 'secure');
+
+// Проверка прав доступа
+if ($ACL->turn(['panel', 'restricted_access'], false)) {
+    $url = preg_replace('#^.*/([^/]+)\.\w{2,5}$#i', "$1", $_SERVER['SCRIPT_NAME'] ?? '');
+    
+    if (!empty($url) && $url !== 'index' && $url !== 'exit') {
+        if (!$ACL->turn(['panel', 'restricted_access_' . $url], false)) {
+            $_SESSION['message'] = __('Permission denied');
+            redirect('/admin/');
+        }
+    }
+}
+
+// Установка модулей
+if (!empty($_GET['install'])) {
+    handleModuleInstallation();
+}
 
 /**
- * Install new a modules.
- * If you has put a new module files to modules directory,
- * you can find that module in the left-side menu.
- * Choose "install" in the module dropdown menu to start install process.
- *
- * During the installation process a some files and settings will be import
- * to the Atom.
- * For more information see FpsModuleInstaller class.
+ * Обработка формы логина
  */
-if (!empty($_GET['install'])) {
-	$instMod = (string)$_GET['install'];
-	if (!empty($instMod) && preg_match('#^[a-z]+$#i', $instMod)) {
-		$ModulesInstaller = new FpsModuleInstaller();
-		try {
-            $ModulesInstaller->installModule($instMod);
-            $_SESSION['message'] = sprintf(__('Module "%s" has been installed'), $instMod);
-        } catch (Exception $e) {
-            $_SESSION['errors'] = sprintf(__('Module "%s" has been not installed (Reason: %s)'), $instMod, $e->getMessage());
-        }
-        redirect('/admin/');
-	}
-}
-
-
-
-
-
-
-function getAdmFrontMenuParams()
+function handleLoginForm($FpsDB, $ACL): void
 {
-    $out = array();
-    $modules = glob(ROOT . '/modules/*', GLOB_ONLYDIR);
-    if (count($modules)) {
-        foreach ($modules as $key => $modPath) {
-            if (file_exists($modPath . '/info.php')) {
-                include($modPath . '/info.php');
-                if (isset($menuInfo)) {
-                    $mod = basename($modPath);
-                    $out[$mod] = $menuInfo;
+    $errors = [];
+    
+    if (isset($_POST['send'], $_POST['login'], $_POST['passwd'])) {
+        $login = strtolower(trim((string)$_POST['login']));
+        $pass = trim((string)$_POST['passwd']);
+        
+        // Валидация
+        if (empty($login)) {
+            $errors[] = 'Заполните поле "Логин"';
+        }
+        if (empty($pass)) {
+            $errors[] = 'Заполните поле "Пароль"';
+        }
+        
+        if (empty($errors)) {
+            // Защищенный запрос с подготовленными statement
+            $user = $FpsDB->select('users', DB_FIRST, [
+                'cond' => [
+                    'name' => $login, 
+                    'passw' => md5($pass)
+                ]
+            ]);
+            
+            if (empty($user)) {
+                $errors[] = 'Не верный Пароль или Логин';
+            } else {
+                $ACL->turn(['panel', 'entry'], true, (int)$user[0]['status']);
+                
+                if (empty($errors)) {
+                    $_SESSION['user'] = $user[0];
+                    $_SESSION['adm_panel_authorize'] = time() + (int)Config::read('session_time', 'secure');
+                    redirect('/admin/');
                 }
             }
         }
     }
+    
+    renderLoginForm($errors);
+}
+
+/**
+ * Рендер формы логина
+ */
+function renderLoginForm(array $errors = []): void
+{
+    $pageTitle = 'Авторизация в панели Администрирования';
+    ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AtomX Admin Panel Authorization</title>
+    <meta name="description" content="Панель администратора CMS AtomX">
+    <meta name="keywords" content="AtomX, CMS, admin panel">
+    <link rel="stylesheet" type="text/css" href="<?= htmlspecialchars(WWW_ROOT) ?>/admin/template/css/style.css">
+    <script src="<?= htmlspecialchars(WWW_ROOT) ?>/sys/js/jquery.js"></script>
+    <script>
+    $(document).ready(function(){
+        const shmask = $('.shadow-mask');
+        if (shmask.length > 0) {
+            const bodyWidth = $('body').width() || 0;
+            let lpos = (bodyWidth - 900) / 2;
+            if (lpos < 1) lpos = 0;
+            
+            const l = lpos % 18;
+            const finalLeft = lpos + (18 - l) + 51;
+            shmask.css('left', finalLeft);
+        }
+    });
+    </script>
+</head>
+<body>
+    <div id="login-wrapper">
+        <div class="shadow-mask"></div>
+        <div class="form">
+            <div class="title">Авторизация</div>
+            <form method="POST" action="">
+                <div class="items">
+                    <?php if (!empty($errors)): ?>
+                    <ul class="error">
+                        <?php foreach ($errors as $error): ?>
+                        <li><?= htmlspecialchars($error) ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php endif; ?>
+                    
+                    <div class="item">
+                        <span>Логин</span>
+                        <input name="login" type="text" required>
+                    </div>
+                    <div class="item">
+                        <span>Пароль</span>
+                        <input name="passwd" type="password" required>
+                    </div>
+                </div>
+                <div class="submit">
+                    <input type="submit" name="send" value="Войти">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+    <?php
+}
+
+/**
+ * Обработка установки модулей
+ */
+function handleModuleInstallation(): void
+{
+    $instMod = (string)$_GET['install'];
+    
+    if (!empty($instMod) && preg_match('#^[a-z]+$#i', $instMod)) {
+        $ModulesInstaller = new FpsModuleInstaller();
+        
+        try {
+            $ModulesInstaller->installModule($instMod);
+            $_SESSION['message'] = sprintf(__('Module "%s" has been installed'), $instMod);
+        } catch (Exception $e) {
+            $_SESSION['errors'] = sprintf(
+                __('Module "%s" has been not installed (Reason: %s)'), 
+                $instMod, 
+                $e->getMessage()
+            );
+        }
+        
+        redirect('/admin/');
+    }
+}
+
+/**
+ * Получение параметров меню админки
+ */
+function getAdmFrontMenuParams(): array
+{
+    $out = [];
+    $modules = glob(ROOT . '/modules/*', GLOB_ONLYDIR);
+    
+    if ($modules === false) {
+        return $out;
+    }
+    
+    foreach ($modules as $modPath) {
+        $infoFile = $modPath . '/info.php';
+        
+        if (file_exists($infoFile)) {
+            $menuInfo = [];
+            include($infoFile);
+            
+            if (isset($menuInfo) && is_array($menuInfo)) {
+                $mod = basename($modPath);
+                $out[$mod] = $menuInfo;
+            }
+        }
+    }
+    
     return $out;
 }
 
